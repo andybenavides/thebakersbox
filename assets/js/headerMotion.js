bdc6a3678557584d15c67a8c309b13e8f874a11b
Andy Benavides <andybenavides.22@gmail.com>
2016-03-17 15:01:10 -0700

    Complete nav overhaul

--- assets/js/headerMotion.js
+++ assets/js/headerMotion.js
@@ -1,29 +1,7 @@
 ///**
 // * Created by andybenavides on 1/21/16.
 // */
-//
-//$(document).on("scroll", function(){
-//
-//    var logoheader = document.getElementById("logo");
-//    var links = document.getElementsByClassName("MenuLink");
-//
-//    if ($(document).scrollTop() > 170){
-//        $("header").addClass("shrink");
-//        logoheader.innerHTML = "bakers<br>box";
-//        for(i=0; i<6; i++) {
-//            $(links[i]).addClass("MenuLinkStyle");
-//        }
-//        logoheader.style.color = "#002833";
-//    }
-//    else {
-//        $("header").removeClass("shrink");
-//        logoheader.innerHTML = "the<br>bakers<br>box<br>cafe";
-//        for(i=0; i<6; i++) {
-//            $(links[i]).removeClass("MenuLinkStyle");
-//        }
-//        logoheader.style.color = "white";
-//    }
-//});
+
 
 $(document).ready(function(){
     $(".contactForm").hide();
@@ -31,20 +9,8 @@ $(document).ready(function(){
         $(".contactForm").slideToggle(300);
     });
 
-
-    //if (document.readyState === "complete") {
-    //    imageChange();
-    //};
+    $(window).scroll(function(){
+        $(".mainHeader").css("opacity", 1 - $(window).scrollTop() / 350);
+    });
 });
 
-
-//fade out on scroll
-//$(window).scroll(function(){
-//    $(".indentSection").css("opacity", 1 - $(window).scrollTop() / 250);
-//})
-//
-//$(window).load(function(){
-//    $("header").css("opacity", 0 + $(window).scrollTop() / 150);
-//});
-//
-//
