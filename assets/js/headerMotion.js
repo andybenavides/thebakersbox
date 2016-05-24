0283e79d40829b5f5750004b32959884bce94426
Andy Benavides <andybenavides.22@gmail.com>
2016-05-23 23:23:02 -0700

    finished header and main page

--- assets/js/headerMotion.js
+++ assets/js/headerMotion.js
@@ -3,10 +3,36 @@
 // */
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".contactForm").hide();
-    $("#mail").click(function(){
+    $("#mail").click(function () {
         $(".contactForm").slideToggle(300);
     });
 });
 
+
+$(document).on("scroll", function () {
+
+    var links = document.getElementsByClassName("MenuLink");
+    var logo = document.getElementById("logo");
+    var mainHeader = document.getElementById("mainHeader");
+
+    if ($(document).scrollTop() > 150) {
+        $("header").addClass("shrink");
+        $(logo).show();
+        for (i = 0; i < 6; i++) {
+            $(links[i]).addClass("MenuLinkStyle");
+        }
+        logo.style.color = "black";
+        $(mainHeader).fadeOut();
+    }
+    else {
+        $("header").removeClass("shrink");
+        $(logo).hide();
+        for (i = 0; i < 6; i++) {
+            $(links[i]).removeClass("MenuLinkStyle");
+        }
+        logo.style.color = "white";
+        $(mainHeader).fadeIn();
+    }
+});
